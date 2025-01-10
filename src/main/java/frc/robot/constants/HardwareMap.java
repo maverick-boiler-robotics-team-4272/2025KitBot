179d1916ccee441fefe10df7ec9727c3e95ef60e
cland29 <edlazars@gmail.com>
2025-01-09 19:38:51 -0500

    add dropper

---
+++ src/main/java/frc/robot/constants/HardwareMap.java
@@ -0,0 +1,5 @@
+package frc.robot.constants;
+
+public class HardwareMap {
+    public static final int DROPPER_MOTOR_ID = 0;
+}
--- src/main/java/frc/robot/subsystems/dropper/Dropper.java
+++ src/main/java/frc/robot/subsystems/dropper/Dropper.java
@@ -4,11 +4,26 @@ import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.constants.HardwareMap;
+import frc.robot.utils.hardware.VortexBuilder;
 
 public class Dropper implements Subsystem {
-    SparkFlex dropperMotor;
+    
+
+    private SparkFlex dropperMotor;
+
+    
 
     public Dropper() {
-        dropperMotor = new SparkFlex(0, MotorType.kBrushless);
+        
+        dropperMotor = VortexBuilder.create(HardwareMap.DROPPER_MOTOR_ID)
+            .withCurrentLimit(80)
+            .withVoltageCompensation(12)
+            .withInversion(true)
+            .build();
+    }
+
+    public void setPower(double power) {
+        dropperMotor.set(power);
     }
 }
