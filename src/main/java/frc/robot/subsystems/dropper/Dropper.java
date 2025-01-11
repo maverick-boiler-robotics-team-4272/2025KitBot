031297bfd4645c57d96aac23a6379ce811a93f5b
Andrew Lohr <lohrandrew@icloud.com>
2025-01-11 09:24:34 -0500

    Add the dropper lidar

--- src/main/java/frc/robot/subsystems/dropper/Dropper.java
+++ src/main/java/frc/robot/subsystems/dropper/Dropper.java
@@ -1,21 +1,19 @@
 package frc.robot.subsystems.dropper;
 
 import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.playingwithfusion.TimeOfFlight;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.constants.HardwareMap;
 import frc.robot.utils.hardware.VortexBuilder;
 
 public class Dropper implements Subsystem {
     
-
     private SparkFlex dropperMotor;
+    TimeOfFlight lidar = new TimeOfFlight(0);
 
-    
-
-    public Dropper() {
-        
+    public Dropper() {  
         dropperMotor = VortexBuilder.create(HardwareMap.DROPPER_MOTOR_ID)
             .withCurrentLimit(80)
             .withVoltageCompensation(12)
@@ -25,5 +23,14 @@ public class Dropper implements Subsystem {
 
     public void setPower(double power) {
         dropperMotor.set(power);
+    }
+
+    public boolean hasCoral() {
+        return lidar.getRange() < 40;
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("Has Coral", hasCoral());
     }
 }
