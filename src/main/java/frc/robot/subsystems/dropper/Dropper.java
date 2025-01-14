25d8f5dd85aca2928d5d1482139711e0973277a9
Andrew Lohr <48129314+alohr86@users.noreply.github.com>
2025-01-13 22:52:04 -0500

    Add logs inputs to the dropper

--- src/main/java/frc/robot/subsystems/dropper/Dropper.java
+++ src/main/java/frc/robot/subsystems/dropper/Dropper.java
@@ -3,16 +3,26 @@ package frc.robot.subsystems.dropper;
 import com.revrobotics.spark.SparkFlex;
 import com.playingwithfusion.TimeOfFlight;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.utils.hardware.VortexBuilder;
+import frc.robot.utils.logging.Loggable;
 
 import static frc.robot.constants.SubsystemConstants.DropperConstants.*;
 
-public class Dropper implements Subsystem {
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
+
+public class Dropper implements Subsystem, Loggable {
+    @AutoLog
+    public static class DropperInputs {
+        public double dropperPower;
+        public boolean lidarTripped;
+    }
+
+    DropperInputsAutoLogged inputs = new DropperInputsAutoLogged();
     
     private SparkFlex dropperMotor;
-    TimeOfFlight lidar = new TimeOfFlight(DROPPER_LIDAR_ID);
+    private TimeOfFlight lidar = new TimeOfFlight(DROPPER_LIDAR_ID);
 
     public Dropper() {  
         dropperMotor = VortexBuilder.create(DROPPER_MOTOR_ID)
@@ -20,9 +30,13 @@ public class Dropper implements Subsystem {
             .withVoltageCompensation(12)
             .withInversion(true)
             .build();
+
+        inputs.dropperPower = 0.0;
+        inputs.lidarTripped = false;
     }
 
     public void setPower(double power) {
+        inputs.dropperPower = power;
         dropperMotor.set(power);
     }
 
@@ -31,7 +45,14 @@ public class Dropper implements Subsystem {
     }
 
     @Override
+    public void log(String subdirectory, String humanReadableName) {
+        Logger.processInputs(subdirectory + "/" + humanReadableName, inputs);
+    }
+
+    @Override
     public void periodic() {
-        SmartDashboard.putBoolean("Has Coral", hasCoral());
+        inputs.lidarTripped = hasCoral();
+
+        log("Subsytems", "Dropper");
     }
 }
