864b77315258baea788f0f16565dfaa84ea8ec4b
DrRobbie <menon.elliott@gmail.com>
2025-01-14 18:45:36 -0500

    did limelight constants

--- src/main/java/frc/robot/constants/SubsystemConstants.java
+++ src/main/java/frc/robot/constants/SubsystemConstants.java
@@ -4,8 +4,11 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.utils.limelight.Limelight;
 
 import static frc.robot.constants.TunerConstants.kSpeedAt12Volts;
 
@@ -35,5 +38,20 @@ public class SubsystemConstants {
 
     public static class DropperConstants {
         
+    }
+
+    public static class LimeLightConstants {
+        public static Pose3d FRONT_LIMELIGHT_POSE = new Pose3d(
+            0.26545,
+            0.167063,
+            0.223797,
+            new Rotation3d(
+                0, 
+                0.3507, // 20 degrees
+                // Rotation2d.fromDegrees(10).getRadians()
+                Rotation2d.fromDegrees(180).getRadians()
+            )
+        );
+        public static Limelight FRONT_LIMELIGHT = new Limelight("limelight-front");
     }
 }
