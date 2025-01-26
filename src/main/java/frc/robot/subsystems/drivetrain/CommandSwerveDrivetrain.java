a2e4a45c0bc0f1004990a6d44ca9320646574e18
Andrew Lohr <48129314+alohr86@users.noreply.github.com>
2025-01-26 17:35:19 -0500

    Move all of the sysId Things to the bottom.

--- src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
@@ -61,31 +61,19 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         inputs.fuseVison = false;
         inputs.estimatedPose = new Pose2d();
         inputs.isRedSide = false;
+
         inputs.moduleStates = new SwerveModuleState[4];
-        for(int i = 0; i < 4; i++) {
-            inputs.moduleStates[i] = getModule(i).getCurrentState();
-        }
-
         inputs.driveCurrents = new double[4];
-        for(int i = 0; i < 4; i++) {
-            inputs.driveCurrents[i] = getModule(i).getDriveMotor().getStatorCurrent().getValueAsDouble();
-        }
-
         inputs.steerCurrents = new double[4];
         for(int i = 0; i < 4; i++) {
+            inputs.moduleStates[i] = getModule(i).getCurrentState();
             inputs.steerCurrents[i] = getModule(i).getSteerMotor().getStatorCurrent().getValueAsDouble();
+            inputs.driveCurrents[i] = getModule(i).getDriveMotor().getStatorCurrent().getValueAsDouble();
         }
 
         FRONT_LIMELIGHT.configure(FRONT_LIMELIGHT_POSE);
         BACK_LIMELIGHT.configure(BACK_LIMELIGHT_POSE);
     }
-
-    //Last distance the tag was read
-    private double lastDistance = 0.0;
-
-    private static final double kSimLoopPeriod = 0.005; // 5 ms
-    private Notifier m_simNotifier = null;
-    private double m_lastSimTime;
 
     /* Blue alliance sees forward as 0 degrees (toward red alliance wall) */
     private static final Rotation2d kBlueAlliancePerspectiveRotation = Rotation2d.kZero;
@@ -99,8 +87,169 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
 
-    /** Swerve request to apply during robot-centric path following */
+    /* Swerve request to apply during robot-centric path following */
     private final SwerveRequest.ApplyRobotSpeeds m_pathApplyRobotSpeeds = new SwerveRequest.ApplyRobotSpeeds();
+
+    /**
+     * Constructs a CTRE SwerveDrivetrain using the specified constants.
+     * <p>
+     * This constructs the underlying hardware devices, so users should not construct
+     * the devices themselves. If they need the devices, they can access them through
+     * getters in the classes.
+     *
+     * @param drivetrainConstants   Drivetrain-wide constants for the swerve drive
+     * @param modules               Constants for each specific module
+     */
+    public CommandSwerveDrivetrain(
+        SwerveDrivetrainConstants drivetrainConstants,
+        SwerveModuleConstants<?, ?, ?>... modules
+    ) {
+        super(drivetrainConstants, modules);
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+
+        initPathPlanner();
+        initInputs();
+    }
+
+    /**
+     * Constructs a CTRE SwerveDrivetrain using the specified constants.
+     * <p>
+     * This constructs the underlying hardware devices, so users should not construct
+     * the devices themselves. If they need the devices, they can access them through
+     * getters in the classes.
+     *
+     * @param drivetrainConstants     Drivetrain-wide constants for the swerve drive
+     * @param odometryUpdateFrequency The frequency to run the odometry loop. If
+     *                                unspecified or set to 0 Hz, this is 250 Hz on
+     *                                CAN FD, and 100 Hz on CAN 2.0.
+     * @param modules                 Constants for each specific module
+     */
+    public CommandSwerveDrivetrain(
+        SwerveDrivetrainConstants drivetrainConstants,
+        double odometryUpdateFrequency,
+        SwerveModuleConstants<?, ?, ?>... modules
+    ) {
+        super(drivetrainConstants, odometryUpdateFrequency, modules);
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+
+        initPathPlanner();
+        initInputs();
+    }      
+
+    /**
+     * Returns a command that applies the specified control request to this swerve drivetrain.
+     *
+     * @param request Function returning the request to apply
+     * @return Command to run
+     */
+    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+        return run(() -> this.setControl(requestSupplier.get()));
+    }
+
+    private void initPathPlanner() {
+        try {
+            var config = RobotConfig.fromGUISettings();
+            AutoBuilder.configure(
+                () -> getState().Pose,   // Supplier of current robot pose
+                this::resetPose,         // Consumer for seeding pose against auto
+                () -> getState().Speeds, // Supplier of current robot speeds
+                // Consumer of ChassisSpeeds and feedforwards to drive the robot
+                (speeds, feedforwards) -> setControl(
+                    m_pathApplyRobotSpeeds.withSpeeds(speeds)
+                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
+                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
+                ),
+                new PPHolonomicDriveController(
+                    // PID constants for translation
+                    new PIDConstants(TRANSLATION_P, TRANSLATION_I, TRANSLATION_D),
+                    // PID constants for rotation
+                    new PIDConstants(ROTATION_P, ROTATION_I, ROTATION_D)
+                ),
+                config,
+                // Assume the path needs to be flipped for Red vs Blue, this is normally the case
+                () -> FieldConstants.isRedSide(),
+                this // Subsystem for requirements
+            );
+        } catch (Exception ex) {
+            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
+        }
+    }
+
+    private void fuseOdometry() {
+        FRONT_LIMELIGHT.setRobotOrientation(getState().Pose.getRotation().getDegrees());
+        BACK_LIMELIGHT.setRobotOrientation(getState().Pose.getRotation().getDegrees());
+
+        fuseVision(BACK_LIMELIGHT.getBotPoseEstimate());
+        fuseVision(FRONT_LIMELIGHT.getBotPoseEstimate());
+    }
+    
+    public void fuseVision(LimelightHelpers.PoseEstimate limelightMeasurement) {
+        if(limelightMeasurement != null) {
+            if(
+                limelightMeasurement.tagCount > 0 && 
+                limelightMeasurement.avgTagDist < 2.0 && 
+                Units.radiansToRotations(getState().Speeds.omegaRadiansPerSecond) < 2.0
+            ) {
+                setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
+                addVisionMeasurement(
+                    limelightMeasurement.pose,
+                    Utils.fpgaToCurrentTime(limelightMeasurement.timestampSeconds)
+                );
+
+                inputs.fuseVison = true;
+            } else {
+                inputs.fuseVison = false;
+            }
+        }
+    }
+
+    @Override
+    public void log(String subdirectory, String humanReadableName) {
+        Logger.processInputs(subdirectory + "/" + humanReadableName, inputs);
+    }
+
+    @Override
+    public void periodic() {
+        boolean isRed = FieldConstants.isRedSide();
+
+        /*
+         * Periodically try to apply the operator perspective.
+         * If we haven't applied the operator perspective before, then we should apply it regardless of DS state.
+         * This allows us to correct the perspective in case the robot code restarts mid-match.
+         * Otherwise, only check and apply the operator perspective if the DS is disabled.
+         * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
+         */
+        if (!m_hasAppliedOperatorPerspective) {
+            setOperatorPerspectiveForward(
+                isRed
+                    ? kRedAlliancePerspectiveRotation
+                    : kBlueAlliancePerspectiveRotation
+            );
+        }
+
+        inputs.isRedSide = isRed;
+
+        log("Subsystems", "Drivetrain");
+
+        fuseOdometry();
+
+        inputs.estimatedPose = getState().Pose;
+        inputs.moduleStates = getState().ModuleStates;
+
+        for(int i = 0; i < 4; i++) {
+            inputs.driveCurrents[i] = getModule(i).getDriveMotor().getStatorCurrent().getValueAsDouble();
+            inputs.steerCurrents[i] = getModule(i).getSteerMotor().getStatorCurrent().getValueAsDouble();
+        }
+    }
+
+    
+    private static final double kSimLoopPeriod = 0.005; // 5 ms
+    private Notifier m_simNotifier = null;
+    private double m_lastSimTime;
 
     /* SysId routine for characterizing translation. This is used to find PID gains for the drive motors. */
     private final SysIdRoutine m_sysIdRoutineTranslation = new SysIdRoutine(
@@ -165,66 +314,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
 
     /**
-     * Constructs a CTRE SwerveDrivetrain using the specified constants.
-     * <p>
-     * This constructs the underlying hardware devices, so users should not construct
-     * the devices themselves. If they need the devices, they can access them through
-     * getters in the classes.
-     *
-     * @param drivetrainConstants   Drivetrain-wide constants for the swerve drive
-     * @param modules               Constants for each specific module
-     */
-    public CommandSwerveDrivetrain(
-        SwerveDrivetrainConstants drivetrainConstants,
-        SwerveModuleConstants<?, ?, ?>... modules
-    ) {
-        super(drivetrainConstants, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
-
-        initPathPlanner();
-        initInputs();
-    }
-
-    /**
-     * Constructs a CTRE SwerveDrivetrain using the specified constants.
-     * <p>
-     * This constructs the underlying hardware devices, so users should not construct
-     * the devices themselves. If they need the devices, they can access them through
-     * getters in the classes.
-     *
-     * @param drivetrainConstants     Drivetrain-wide constants for the swerve drive
-     * @param odometryUpdateFrequency The frequency to run the odometry loop. If
-     *                                unspecified or set to 0 Hz, this is 250 Hz on
-     *                                CAN FD, and 100 Hz on CAN 2.0.
-     * @param modules                 Constants for each specific module
-     */
-    public CommandSwerveDrivetrain(
-        SwerveDrivetrainConstants drivetrainConstants,
-        double odometryUpdateFrequency,
-        SwerveModuleConstants<?, ?, ?>... modules
-    ) {
-        super(drivetrainConstants, odometryUpdateFrequency, modules);
-        if (Utils.isSimulation()) {
-            startSimThread();
-        }
-
-        initPathPlanner();
-        initInputs();
-    }      
-
-    /**
-     * Returns a command that applies the specified control request to this swerve drivetrain.
-     *
-     * @param request Function returning the request to apply
-     * @return Command to run
-     */
-    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
-        return run(() -> this.setControl(requestSupplier.get()));
-    }
-
-    /**
      * Runs the SysId Quasistatic test in the given direction for the routine
      * specified by {@link #m_sysIdRoutineToApply}.
      *
@@ -244,103 +333,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
      */
     public Command sysIdDynamic(SysIdRoutine.Direction direction) {
         return m_sysIdRoutineToApply.dynamic(direction);
-    }
-
-    private void initPathPlanner() {
-        try {
-            var config = RobotConfig.fromGUISettings();
-            AutoBuilder.configure(
-                () -> getState().Pose,   // Supplier of current robot pose
-                this::resetPose,         // Consumer for seeding pose against auto
-                () -> getState().Speeds, // Supplier of current robot speeds
-                // Consumer of ChassisSpeeds and feedforwards to drive the robot
-                (speeds, feedforwards) -> setControl(
-                    m_pathApplyRobotSpeeds.withSpeeds(speeds)
-                        .withWheelForceFeedforwardsX(feedforwards.robotRelativeForcesXNewtons())
-                        .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())
-                ),
-                new PPHolonomicDriveController(
-                    // PID constants for translation
-                    new PIDConstants(TRANSLATION_P, TRANSLATION_I, TRANSLATION_D),
-                    // PID constants for rotation
-                    new PIDConstants(ROTATION_P, ROTATION_I, ROTATION_D)
-                ),
-                config,
-                // Assume the path needs to be flipped for Red vs Blue, this is normally the case
-                () -> FieldConstants.isRedSide(),
-                this // Subsystem for requirements
-            );
-        } catch (Exception ex) {
-            DriverStation.reportError("Failed to load PathPlanner config and configure AutoBuilder", ex.getStackTrace());
-        }
-    }
-
-    private void fuseOdometry() {
-        FRONT_LIMELIGHT.setRobotOrientation(getState().Pose.getRotation().getDegrees());
-        BACK_LIMELIGHT.setRobotOrientation(getState().Pose.getRotation().getDegrees());
-
-        fuseVision(BACK_LIMELIGHT.getBotPoseEstimate());
-        fuseVision(FRONT_LIMELIGHT.getBotPoseEstimate());
-    }
-    
-    public void fuseVision(LimelightHelpers.PoseEstimate limelightMeasurement) {
-        if(limelightMeasurement != null) {
-            if(
-                limelightMeasurement.tagCount > 0 && 
-                limelightMeasurement.avgTagDist < 2.0 && 
-                Units.radiansToRotations(getState().Speeds.omegaRadiansPerSecond) < 2.0
-            ) {
-                setVisionMeasurementStdDevs(VecBuilder.fill(.7,.7,9999999));
-                addVisionMeasurement(
-                    limelightMeasurement.pose,
-                    Utils.fpgaToCurrentTime(limelightMeasurement.timestampSeconds)
-                );
-
-                lastDistance = inputs.distanceTraveled;
-                inputs.fuseVison = true;
-            } else {
-                inputs.fuseVison = false;
-            }
-        }
-    }
-
-    @Override
-    public void log(String subdirectory, String humanReadableName) {
-        Logger.processInputs(subdirectory + "/" + humanReadableName, inputs);
-    }
-
-    @Override
-    public void periodic() {
-        boolean isRed = FieldConstants.isRedSide();
-
-        /*
-         * Periodically try to apply the operator perspective.
-         * If we haven't applied the operator perspective before, then we should apply it regardless of DS state.
-         * This allows us to correct the perspective in case the robot code restarts mid-match.
-         * Otherwise, only check and apply the operator perspective if the DS is disabled.
-         * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
-         */
-        if (!m_hasAppliedOperatorPerspective) {
-            setOperatorPerspectiveForward(
-                isRed
-                    ? kRedAlliancePerspectiveRotation
-                    : kBlueAlliancePerspectiveRotation
-            );
-        }
-
-        inputs.isRedSide = isRed;
-
-        log("Subsystems", "Drivetrain");
-
-        fuseOdometry();
-
-        inputs.estimatedPose = getState().Pose;
-        inputs.moduleStates = getState().ModuleStates;
-
-        for(int i = 0; i < 4; i++) {
-            inputs.driveCurrents[i] = getModule(i).getDriveMotor().getStatorCurrent().getValueAsDouble();
-            inputs.steerCurrents[i] = getModule(i).getSteerMotor().getStatorCurrent().getValueAsDouble();
-        }
     }
 
     private void startSimThread() {
