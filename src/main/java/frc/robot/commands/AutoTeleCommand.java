b3868ccf856ce1ae56bd2f123442d08eaed1ae2c
Andrew Lohr <lohrandrew@icloud.com>
2025-01-28 13:26:50 -0500

    Add lidar drop state to the autogamecommand

--- src/main/java/frc/robot/commands/AutoTeleCommand.java
+++ src/main/java/frc/robot/commands/AutoTeleCommand.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.subsystems.drivetrain.states.PathfindingState;
 import frc.robot.subsystems.dropper.Dropper;
+import frc.robot.subsystems.dropper.states.LidarDropState;
 import frc.robot.subsystems.dropper.states.LidarWaitState;
 
 public class AutoTeleCommand extends SequentialCommandGroup {
     public AutoTeleCommand(CommandSwerveDrivetrain drivetrain, Dropper dropper, Supplier<Pose2d> scorePose, Supplier<Pose2d> feedPose) {
         super(
             new PathfindingState(drivetrain, scorePose),
+            new LidarDropState(dropper),
             new PathfindingState(drivetrain, feedPose),
             new LidarWaitState(dropper)
         );
