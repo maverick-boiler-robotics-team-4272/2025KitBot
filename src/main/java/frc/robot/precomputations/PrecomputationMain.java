64d547044c854b747327c3424b15def3579cd542
Andrew Lohr <lohrandrew@icloud.com>
2025-01-28 13:27:06 -0500

    Fix filereader

--- src/main/java/frc/robot/precomputations/PrecomputationMain.java
+++ src/main/java/frc/robot/precomputations/PrecomputationMain.java
@@ -1,21 +1,20 @@
 package frc.robot.precomputations;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class PrecomputationMain {
     public static void main(String[] args) {
         System.out.println("Testing pre-computations");
 
-        String content;
 
-        try {
-            content = Files.readString(Path.of("./src/main/deploy/pathplanner/paths/Finding.path"));
+        try(BufferedReader reader = new BufferedReader(new FileReader("./src/main/deploy/pathplanner/paths/Finding.path"))) {
         } catch(Exception e) {
             System.out.println("An error occurred reading the file... Be sure to make a path file named Finding!");
             throw new RuntimeException(e);
-        }
+        } 
 
-        System.out.println(content);
+        // JSONParser parser = new JSONParser();
+        // // JSONObject obj = (JSONObject)parser.parse(reader);
     }   
 }
