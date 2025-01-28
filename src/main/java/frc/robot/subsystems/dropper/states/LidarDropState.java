6fe6496a07360e6adfcf16f01eed27cd20f5fda7
Andrew Lohr <lohrandrew@icloud.com>
2025-01-28 13:26:35 -0500

    Make lidar drop state

---
+++ src/main/java/frc/robot/subsystems/dropper/states/LidarDropState.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.dropper.states;
+
+import frc.robot.subsystems.dropper.Dropper;
+import frc.robot.utils.commandUtils.State;
+
+public class LidarDropState extends State<Dropper> {
+    public LidarDropState(Dropper dropper) {
+        super(dropper);
+    }
+
+    @Override
+    public void initialize() {
+        requiredSubsystem.setPower(0.25);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !requiredSubsystem.hasCoral();
+    }
+}
