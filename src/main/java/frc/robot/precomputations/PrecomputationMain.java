37129a27a98463f1d7fdda1f765ed53bf43aa300
Andrew Lohr <48129314+alohr86@users.noreply.github.com>
2025-01-31 14:17:27 -0500

    Create the pathfindingposeparser methods and call them in the precomputations main

--- src/main/java/frc/robot/precomputations/PrecomputationMain.java
+++ src/main/java/frc/robot/precomputations/PrecomputationMain.java
@@ -1,20 +1,9 @@
 package frc.robot.precomputations;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class PrecomputationMain {
     public static void main(String[] args) {
         System.out.println("Testing pre-computations");
 
-
-        try(BufferedReader reader = new BufferedReader(new FileReader("./src/main/deploy/pathplanner/paths/Finding.path"))) {
-        } catch(Exception e) {
-            System.out.println("An error occurred reading the file... Be sure to make a path file named Finding!");
-            throw new RuntimeException(e);
-        } 
-
-        // JSONParser parser = new JSONParser();
-        // // JSONObject obj = (JSONObject)parser.parse(reader);
-    }   
+        PathfindingPoseParser.editPathfindingPoses();
+    }
 }
